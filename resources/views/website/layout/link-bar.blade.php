740b770f2db64bc3d4831302596ff036f9c98912
Biswajit Laskar <biswajit2013laskar@gmail.com>
2023-06-19 16:41:55 +0530

    asdasdas

--- resources/views/website/layout/link-bar.blade.php
+++ resources/views/website/layout/link-bar.blade.php
@@ -76,12 +76,12 @@
                     </div>
                 </div>
                 <div class="col-xs-4 col-sm-4 col-md-4">
-                    <div class="widget no-border m-0 mobile-contact-info">
+                    {{-- <div class="widget no-border m-0 mobile-contact-info">
                       <div class="mt-10 mb-10 flip sm-text-center gigw">
                           <div class="font-15 text-black-333 mb-5 font-weight-600"><i class="fa fa-map-marker text-theme-colored font-18"></i> Our Location</div>
                           <a href="#" class="font-12 text-gray">Amingaon, Guwahati</a>
                       </div>
-                    </div>
+                    </div> --}}
                 </div>
                 <div class="col-xs-4 col-sm-4 col-md-4">
                     <div class="widget no-border m-0 mobile-contact-info">
@@ -92,7 +92,8 @@
                           <li><a href="#"><i class="fa fa-twitter"></i></a></li>
                           <li><a href="#"><i class="fa fa-instagram"></i></a></li>
                         </ul>
-                        <a href="#" class="font-12 text-gray social">Our Social Media Handles</a>
+                        <a href="#" class="font-12 text-gray">Amingaon, Guwahati</a>
+                        {{-- <a href="#" class="font-12 text-gray social">Our Social Media Handles</a> --}}
                     </div>
                     </div>
                 </div>

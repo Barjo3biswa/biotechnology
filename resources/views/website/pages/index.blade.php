1b4566e6c65a5eb742f11271304fe953992a91e8
Biswajit Laskar <biswajit2013laskar@gmail.com>
2023-06-19 17:42:48 +0530

    ewrwr

--- resources/views/website/pages/index.blade.php
+++ resources/views/website/pages/index.blade.php
@@ -893,7 +893,15 @@
 <script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.parallax.min.js"></script>
 <script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js"></script>
 <script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.video.min.js"></script>
-
+{{-- <script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script> --}}
+<script>
+    $( document ).ready(function() {
+        console.log("ready!");
+    });
+</script>
   <script>
     // function increaseFont() {
     //   var body = document.getElementsByTagName("body")[0];
